b5c5ff182a0801c9d3efb8b728c72195ffbcc705
KMZT <81222923+KMZT@users.noreply.github.com>
2024-09-27 21:59:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ session = requests.session()
 session.cookies['.ROBLOSECURITY'] = settings["cookie"]
 
 token = None
-payload = [{ "itemType": "Asset", "id": id } for id in item_ids]
+# Изменяем itemType на GamePass
+payload = [{"itemType": "GamePass", "id": id} for id in item_ids]
 cache = []
 
 logs = []
@@ -34,55 +35,57 @@ def _set_auth():
     global token, session
     try:
         conn = session.post("https://auth.roblox.com/v2/logout")
-        if conn.headers.get("x-csrf-token"):
+                if conn.headers.get("x-csrf-token"):
             token = conn.headers["x-csrf-token"]
     except:
         time.sleep(5)
         return _set_auth()
 
-def get_product_id(id):
+def get_gamepass_product_id(id):
     try:
-        conn = session.get(f"https://economy.roblox.com/v2/assets/{id}/details", verify=False)
+        # Получение данных о GamePass
+        conn = session.get(f"https://www.roblox.com/game-pass/{id}", verify=False)
         data = conn.json()
 
         if conn.status_code == 200:
-            return  {
-                "id": data["ProductId"],
-                "creator": data["Creator"]["Id"]
+            return {
+                "id": data["ProductId"],  # ID продукта для покупки
+                "creator": data["Creator"]["Id"]  # ID создателя GamePass
             }
         else:
             time.sleep(1)
-            return get_product_id(id)
+            return get_gamepass_product_id(id)
     except:
         time.sleep(1)
-        return get_product_id(id)
+        return get_gamepass_product_id(id)
 
-def buy_item(product_id, seller_id, price):
+def buy_gamepass(product_id, seller_id, price):
     global logs
 
     try:
         body = {
-            "expectedCurrency": 1,
-            "expectedPrice": price,
-            "expectedSellerId": seller_id
+            "expectedCurrency": 1,  # Валюта, 1 = Robux
+            "expectedPrice": price,  # Цена покупки
+            "expectedSellerId": seller_id  # ID продавца GamePass
         }
         headers = {
             "x-csrf-token": token,
         }
+        # Выполняем запрос на покупку GamePass
         conn = session.post(f"https://economy.roblox.com/v1/purchases/products/{product_id}", headers=headers, json=body)
         data = conn.json()
         if conn.status_code == 200:
             if ("purchased" in data) and data["purchased"] == True:
                 purchase_time = time.strftime('%H:%M', time.localtime())
-                logs.append(f"✅ Bought:[{product_id}] {item['name']} : [💸{price} Robux] at {purchase_time}")
+                logs.append(f"✅ Bought GamePass:[{product_id}] : [💸{price} Robux] at {purchase_time}")
 
-                # Remove the item from the warnings dictionary
+                # Удаляем элемент из предупреждений
                 if product_id in item_warnings:
                     del item_warnings[product_id]
         else:
-            return buy_item(product_id, seller_id, price)
+            return buy_gamepass(product_id, seller_id, price)
     except:
-        return buy_item(product_id, seller_id, price)
+        return buy_gamepass(product_id, seller_id, price)
 
 def status_update():
     global checks, logs, item_ids
@@ -98,10 +101,10 @@ def status_update():
         print(Fore.CYAN + f"☑️ Checks: {checks}" + Style.RESET_ALL)
         print(Fore.LIGHTCYAN_EX + f"🕒 Elapsed Time: {elapsed_time_str}" + Style.RESET_ALL)
 
-        # Print item IDs and prices
-        print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "🛒 Item IDs and 💲Prices:")
+        # Печать ID предметов и цен
+        print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "🛒 GamePass IDs and 💲Prices:")
         for item_id, price in item_ids.items():
-            print(Fore.LIGHTGREEN_EX + f"🆔 Item ID: [{item_id}] 💸Price: {price} Robux")
+            print(Fore.LIGHTGREEN_EX + f"🆔 GamePass ID: [{item_id}] 💸Price: {price} Robux")
         print(Style.RESET_ALL)
 
         print(Fore.GREEN + Style.BRIGHT + "🪵 Logs:")
@@ -120,7 +123,8 @@ def watcher():
                 "cache-control": "no-cache",
                 "pragma": "no-cache",
             }
-            conn = session.post("https://catalog.roblox.com/v1/catalog/items/details", json={ "items": payload }, headers=headers, verify=False)
+            # Выполняем запрос на проверку деталей GamePass
+            conn = session.post("https://catalog.roblox.com/v1/catalog/items/details", json={"items": payload}, headers=headers, verify=False)
 
             data = conn.json()
             if conn.status_code == 200:
@@ -134,13 +138,13 @@ def watcher():
                             purchase_price = item_ids.get(str(item_id))
                             if purchase_price is not None:
                                 if item["price"] > purchase_price:
-                                    # Check if the item has already been warned
+                                    # Проверка, был ли уже предупрежден о высоком ценнике
                                     if item_id not in item_warnings:
                                         item_warnings[item_id] = False
                                         logs.append(Fore.RED + Style.BRIGHT + f"❌ [{item_id}] {item_name} : The price {item['price']} is higher than the one set {purchase_price}" + Style.RESET_ALL)
                                 else:
                                     cache.append(item_id)
-                                    r_data = get_product_id(item_id)
+                                    r_data = get_gamepass_product_id(item_id)
                                     logs.append(Fore.RED + Style.BRIGHT + f"❌ [{item_id}] {item_name}" + Style.RESET_ALL)
 
                                     price = item["price"]
@@ -148,11 +152,11 @@ def watcher():
 
                                     try:
                                         purchase_time = time.strftime('%H:%M', time.localtime())
-                                        buy_item(r_data["id"], r_data["creator"], price)
+                                        buy_gamepass(r_data["id"], r_data["creator"], price)
                                     except Exception as error:
                                         purchase_time = "Failed to buy"
 
-                                    logs[-1] = Fore.GREEN + Style.BRIGHT + f"✅ Bought:[{item_id}] {item_name} : [💸{price} Robux] at {purchase_time}" + Style.RESET_ALL
+                                    logs[-1] = Fore.GREEN + Style.BRIGHT + f"✅ Bought GamePass:[{item_id}] {item_name} : [💸{price} Robux] at {purchase_time}" + Style.RESET_ALL
                             else:
                                 logs.append(Fore.RED + Style.BRIGHT + f"❌ [{item_id}] {item_name} : Purchase price not set in settings.json" + Style.RESET_ALL)
             elif conn.status_code == 403:
@@ -165,7 +169,7 @@ def watcher():
 if __name__ == '__main__':
     threading.Thread(target=refresh_tokens).start()
     print(Fore.YELLOW + Style.BRIGHT + "🛈 Waiting to fetch token, update cookies and restart if it takes too long" + Style.RESET_ALL)
-    while token == None:
+    while token is None:
         time.sleep(1)
     print(Fore.YELLOW + Style.BRIGHT + "🎉 Fetched token" + Style.RESET_ALL)
     threading.Thread(target=status_update).start()
